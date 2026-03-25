4e856e823a2c5eb16ce61d6bd583491fb47d83b7
adibbezo <adibezzairii@gmail.com>
2026-03-25 13:53:28 +0800

    Add files via upload

---
+++ BalikKampungRush/level3intro.js
@@ -0,0 +1,31 @@
+class level3intro extends Phaser.Scene {
+
+    constructor() {
+        super({
+            key: 'level3intro'
+        });
+    }
+
+    preload() {
+        this.load.image("level3intro", "assets/level3intro.jpg");
+    }
+
+    create() {
+        console.log('*** level3intro scene');
+
+        this.add.image(320, 320, 'level3intro')
+
+        var spaceDown = this.input.keyboard.addKey('SPACE');
+       
+        spaceDown.on('down', function () {
+            console.log('Jump to room3 scene');
+            this.scene.start('room3', {});
+        }, this);
+
+        this.add.text(90, 600, 'Press spacebar to continue', {
+            font: '30px Courier',
+            fill: '#FFFFFF'
+        });
+
+    }
+}
